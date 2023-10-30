fe147378b99c338471d0d022862c9b7ad7b75416
Taras21071988 <tsasojlov@icloud.com>
2023-10-30 12:43:34 +0300

    receiving data when entering a name in the form

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-const apiKey = "789bcf6e3a4847ceaed81634233010";
+// Получаем название города из формы
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#inputCity");
+let city;
+
+//Слушаем отправку с формы
+
+form.onsubmit = function (e) {
+  //Отменяем отправку формы
+
+  e.preventDefault();
+  //Получаем значение из input, с помощью trim() - обрезаем пробелы
+
+  city = input.value.trim();
+
+  //Делаем запрос на сервер для получения данных
+
+  const apiKey = "789bcf6e3a4847ceaed81634233010"; //Ключ для Api
+  //Адрес запроса
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  //Выполняем запрос
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.current.temp_c);
+      console.log(data.location.name);
+      console.log(data.location.country);
+    });
 
 
-// http://api.weatherapi.com/v1/current.json?key=789bcf6e3a4847ceaed81634233010&q=London
-
-const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Kharkiv`;
-
-fetch(query)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
-
+};
