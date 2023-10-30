f4dddbae0f0e0c83a3dbedef1c9864786601cafe
Taras21071988 <tsasojlov@icloud.com>
2023-10-30 11:31:40 +0300

    creating a function to get data from api

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const apiKey = "789bcf6e3a4847ceaed81634233010";
+
+
+// http://api.weatherapi.com/v1/current.json?key=789bcf6e3a4847ceaed81634233010&q=London
+
+const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Kharkiv`;
+
+fetch(query)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
